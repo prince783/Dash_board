9f538d485ab15826bdbf3856ae2430bc87a9479c
prince783 <princeojha783@gmail.com>
2025-06-10 12:13:02 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Navbar from './Component/Navbar.jsx'
+ 
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Navbar/>
+  </StrictMode>,
+)
